32eabf6e75897a87b6272ec539594f904f9c1eb3
collinharmon <collin.harmon1@gmail.com>
2020-10-29 11:01:31 -0500

    Moved connection config to external, untracked file. Removed folders/files from git tracking

--- d2addon-back-end/fileupload/handlefile.js
+++ d2addon-back-end/fileupload/handlefile.js
@@ -22,6 +22,7 @@ const testIt = (filepath) =>{
   //this way the main node.js process will simply spawn a child thread for every fileset to deal with
   //it seems good from an abstraction point of view, keep all the file operation business away from the main Node.JS thread.
   //process.send(`java -jar C:\\Users\\colli\\d2reader\\out\\artifacts\\d2reader_jar\\d2reader.jar ${__dirname}\\${filepath}`);
+  //will implement other way and test metrics
   execSync(`java -jar C:\\Users\\colli\\d2reader\\out\\artifacts\\d2reader_jar\\d2reader.jar ${filepath}`, exec_options, (err, stdout, stderr) => {
       console.log("exec");
       process.send("In exec");
